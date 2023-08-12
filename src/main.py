03a4571bb7b58eb8ef628e6338ee4fe881ae5e94
vard0ni <zahargapeev@gmail.com>
2023-08-12 20:13:21 +0700

    add exception handling and test grouping

--- src/main.py
+++ src/main.py
@@ -3,9 +3,17 @@ from typing import Union
 
 class Calculator:
     def divide(self, x: Union[int, float], y: Union[int, float]) -> int | float:
+        # если принимаются не числа, то выдаём ошибку типа
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both arguments should be numeric")
+        # ошибка деления на 0
+        if y == 0:
+            raise ZeroDivisionError("Cannot divide by zero")
         return x / y
 
     def add(self, x: Union[int, float], y: Union[int, float]) -> int | float:
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both arguments should be numeric")
         return x + y
 
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,24 +1,32 @@
 import pytest
+from contextlib import nullcontext as does_not_raise
+
 from src.main import Calculator
 
 
-@pytest.mark.parametrize(
-    "x, y, res",
-    [
-        (1, 2, 0.5),
-        (5, -1, -5),
-    ]
-)
-def test_divide(x, y, res):
-    assert Calculator().divide(x, y) == res
+class TestCalculator:
+    @pytest.mark.parametrize(
+        "x, y, res, expectation",
+        [
+            (1, 2, 0.5, does_not_raise()),
+            (5, -1, -5, does_not_raise()),
+            (5, "-1", -5, pytest.raises(TypeError)),
+            (5, 0, -5, pytest.raises(ZeroDivisionError)),
+        ]
+    )
+    def test_divide(self, x, y, res, expectation):
+        # expectation - с такими ожиданиями, вызовет исключение или нет
+        with expectation:
+            assert Calculator().divide(x, y) == res
 
-
-@pytest.mark.parametrize(
-    "x, y, res",
-    [
-        (1, 2, 3),
-        (5, -1, 4),
-    ]
-)
-def test_add(x, y, res):
-    assert Calculator().add(x, y) == res
+    @pytest.mark.parametrize(
+        "x, y, res, expectation",
+        [
+            (1, 2, 3, does_not_raise()),
+            (5, -1, 4, does_not_raise()),
+            (5, -1, 4, pytest.raises(TypeError)),
+        ]
+    )
+    def test_add(self, x, y, res, expectation):
+        with expectation:
+            assert Calculator().add(x, y) == res
